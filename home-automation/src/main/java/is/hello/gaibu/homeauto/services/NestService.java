9bba6d8a071d7a674fd6159ecaf74a6479f4beba
Josef Norgan <josef@sayhello.com>
2016-10-27 12:13:40 -0700

    Adding structure name to displayed configuration name

--- home-automation/src/main/java/is/hello/gaibu/homeauto/services/NestService.java
+++ home-automation/src/main/java/is/hello/gaibu/homeauto/services/NestService.java
@@ -7,6 +7,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 /**
  * Created by jnorgan on 10/5/16.
@@ -20,5 +21,8 @@ public interface NestService {
 
   @PUT("/devices/thermostats/{thermostat_id}")
   Call<Map<String, Object>> setThermostatState(@Body Map<String, Object> stateValues);
+
+  @GET("/structures/{structure_id}/name")
+  Call<String> getStructureName(@Path("structure_id") final String structureId);
 
 }
