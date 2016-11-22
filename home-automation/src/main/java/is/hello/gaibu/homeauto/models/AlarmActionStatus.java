01f89348c5a951f67703d50e6da486536adb6730
Tim Bart <tim@sayhello.com>
2016-11-22 14:33:39 -0800

    return AlarmActionStatus for better visibility

---
+++ home-automation/src/main/java/is/hello/gaibu/homeauto/models/AlarmActionStatus.java
@@ -0,0 +1,10 @@
+package is.hello.gaibu.homeauto.models;
+
+public enum AlarmActionStatus {
+    OK,
+    REMOTE_ERROR,
+    OFF_OR_LOCKED,
+    INVALID_HVAC,
+    INVALID_TEMP_RANGE,
+    UNKOWN
+}
